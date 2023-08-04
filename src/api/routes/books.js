98ce81ed11a156dab40d883cb7bf330bcdc43e98
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-08-04 13:05:14 +0200

    Controlador de errores en index.js añadido

--- src/api/routes/books.js
+++ src/api/routes/books.js
@@ -7,7 +7,5 @@ router.get("/libros/:id", getLibroPorID)
 router.post("/libro", crearLibro)
 router.put("/libro/:id", actualizarLibro)
 router.delete("/libro/:id", eliminarLibro)
-router.get("*", (req, res) => {
-    res.status(404).json({ error: "Ruta no encontrada" });
-  });
+
 module.exports = router
--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ app.use(express.json());
 const booksRouter = require("./api/routes/books.js")
 app.use("/", booksRouter);
 
+app.use('*', (req, res, next) => {
+  res.status(404).json({ data: 'Página no encontrada' })
+})
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ data: 'Error interno del servidor' })
+})
+
 const PORT = 4001;
 app.listen(PORT, () => {
   console.log(
